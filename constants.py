d50abdf78f1a7c2320872ebfc566a3c246268df8
Клим Кузнецов <klimman2000@gmail.com>
2024-10-03 16:14:26 +0200

    test

--- constants.py
+++ constants.py
@@ -2,5 +2,3 @@ SCOPES = [
          'https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive',
 ]
-CREDENTIALS_FILE = 'centered-loader-437208-j1-bb76db27a662.json'
-EMAIL='klimman2000@gmail.com'
--- training_spreadsheets.py
+++ training_spreadsheets.py
@@ -1,12 +1,34 @@
+# Новый импорт.
+import os
+
+# Новый импорт.
+from dotenv import load_dotenv
 from google.oauth2.service_account import Credentials
 from googleapiclient import discovery
-from constants import SCOPES, CREDENTIALS_FILE, EMAIL
+from constants import SCOPES
+
+
+load_dotenv()
+
+# Не забудьте добавить свой электронный адрес в файл .env.
+EMAIL_USER = os.environ['EMAIL']
+
+info = {
+    'type': os.environ['TYPE'],
+    'project_id': os.environ['PROJECT_ID'],
+    'private_key_id': os.environ['PRIVATE_KEY_ID'],
+    'private_key': os.environ['PRIVATE_KEY'].replace('\\n', '\n'),  # Fixing the newlines
+    'client_email': os.environ['CLIENT_EMAIL'],
+    'client_id': os.environ['CLIENT_ID'],
+    'auth_uri': os.environ['AUTH_URI'],
+    'token_uri': os.environ['TOKEN_URI'],
+    'auth_provider_x509_cert_url': os.environ['AUTH_PROVIDER_X509_CERT_URL'],
+    'client_x509_cert_url': os.environ['CLIENT_X509_CERT_URL']
+}
 
 def auth():
-    # Создаём экземпляр класса Credentials.
-    credentials = Credentials.from_service_account_file(
-                  filename=CREDENTIALS_FILE, scopes=SCOPES)
-    # Создаём экземпляр класса Resource.
+    credentials = Credentials.from_service_account_info(
+        info=info, scopes=SCOPES)
     service = discovery.build('sheets', 'v4', credentials=credentials)
     return service, credentials
 
@@ -41,7 +63,7 @@ def create_spreadsheet(service):
 def set_user_permissions(spreadsheet_id, credentials):
     permissions_body = {'type': 'user',  # Тип учётных данных.
                         'role': 'writer',  # Права доступа для учётной записи.
-                        'emailAddress': EMAIL}  # Ваш личный гугл-аккаунт.
+                        'emailAddress': EMAIL_USER}  # Ваш личный гугл-аккаунт.
 
     # Создаётся экземпляр класса Resource для Google Drive API.
     drive_service = discovery.build('drive', 'v3', credentials=credentials)
@@ -53,7 +75,38 @@ def set_user_permissions(spreadsheet_id, credentials):
         fields='id'
     ).execute()
 
+
+# Новая функция! Тут обновляются данные документа.
+def spreadsheet_update_values(service, spreadsheetId):
+    # Данные для заполнения: выводятся в таблице сверху вниз, слева направо.
+    table_values = [
+        ['Бюджет путешествий'],
+        ['Весь бюджет', '5000'],
+        ['Все расходы', '=SUM(E7:E30)'],
+        ['Остаток', '=B2-B3'],
+        ['Расходы'],
+        ['Описание', 'Тип', 'Кол-во', 'Цена', 'Стоимость'],
+        ['Перелёт', 'Транспорт', '2', '400', '=C7*D7']
+    ]
+
+    # Тело запроса.
+    request_body = {
+        'majorDimension': 'ROWS',
+        'values': table_values
+    }
+    # Формирование запроса к Google Sheets API.
+    request = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheetId,
+        range='Отпуск 2077!A1:F20',
+        valueInputOption='USER_ENTERED',
+        body=request_body
+    )
+    # Выполнение запроса.
+    request.execute()
+
+
+# Вызов функций.
 service, credentials = auth()
-spreadsheet_id = create_spreadsheet(service)
-if spreadsheet_id:
-    set_user_permissions(spreadsheet_id, credentials)
+spreadsheetId = create_spreadsheet(service)
+set_user_permissions(spreadsheetId, credentials)
+spreadsheet_update_values(service, spreadsheetId)
